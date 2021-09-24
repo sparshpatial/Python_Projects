dfb284aa0421898d057b6c35ba0cf0bdc4a8e407
Sparsh Patial <69122849+sparshpatial@users.noreply.github.com>
2021-09-24 11:37:45 +0530

    Update screenshot.py

--- Capturing Screenshot/screenshot.py
+++ Capturing Screenshot/screenshot.py
@@ -5,8 +5,9 @@ import pyscreenshot
   
 # To capture the screen
 image = pyscreenshot.grab()
-  
-# To display the captured screenshot
+
+
+# To display the captured screenshot 
 image.show()
   
 # To save the screenshot
