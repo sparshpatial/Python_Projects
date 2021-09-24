6b86cae7ebb44506c9b50e01eebb9e0fdaaf9992
Sparsh Patial <69122849+sparshpatial@users.noreply.github.com>
2021-09-24 11:36:16 +0530

    Add files via upload

---
+++ Capturing Screenshot/screenshot.py
@@ -0,0 +1,13 @@
+
+# Program to take screenshot
+  
+import pyscreenshot
+  
+# To capture the screen
+image = pyscreenshot.grab()
+  
+# To display the captured screenshot
+image.show()
+  
+# To save the screenshot
+image.save("screenshot.png")
