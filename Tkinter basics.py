2d9404c701cc1a9aa723b56d428520a71cdbca3f
Sparsh Patial <69122849+sparshpatial@users.noreply.github.com>
2021-10-16 09:12:53 +0530

    Add files via upload

---
+++ Tkinter basics.py
@@ -0,0 +1,13 @@
+from tkinter import *
+ 
+# create root window
+root = Tk()
+ 
+# root window title and dimension
+root.title("BASIC TKINTER PROJECT")
+# Set geometry (widthxheight)
+root.geometry('500x500')
+ 
+# all widgets will be here
+# Execute Tkinter
+root.mainloop()
